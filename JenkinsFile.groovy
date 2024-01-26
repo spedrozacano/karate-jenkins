1bcbd31381d1458997fa8dbcbaf7d572325e3677
spedrozacano <pedrozacano@gmail.com>
2024-01-26 10:53:44 -0500

    Mi primer commit con karate

---
+++ JenkinsFile.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Compile Stage') {
+            steps {
+                script {
+                    def mvnHome = tool name: 'maven_3_9_6', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                        bat "${mvnHome}\\bin\\mvn clean compile"
+                    }
+                }
+
+            }
+        }
+
+        stage('Test Stage') {
+            steps {
+                script {
+                    def mvnHome = tool name: 'maven_3_9_6', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                        bat "${mvnHome}\\bin\\mvn test -Dtest=UsersRunner"
+                    }
+                }
+            }
+
+        }
+
+        stage('Cucumber Reports') {
+            steps {
+                cucumber buildStatus: "UNSTABLE",
+                        fileIncludePattern: "**/cucumber.json",
+                        jsonReportDirectory: 'target'
+            }
+        }
+
+    }
+
+}
