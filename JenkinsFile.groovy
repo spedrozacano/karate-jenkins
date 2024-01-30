803528c506b89bfee2b71bdf3864de0c192fd9ae
spedrozacano <157819547+spedrozacano@users.noreply.github.com>
2024-01-30 16:03:05 -0500

    Update JenkinsFile.groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -6,7 +6,7 @@ pipeline {
         stage('Compile Stage') {
             steps {
                 script {
-                    def mvnHome = tool name: 'maven_3_9_5', type: 'maven'
+                    def mvnHome = tool name: 'maven_3_9_6', type: 'maven'
                     withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
                         bat "${mvnHome}\\bin\\mvn clean compile"
                     }
